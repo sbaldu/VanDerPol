91af1df434159b6b579da40342dc4d2f57772570
sbaldu <simone.balducci00@gmail.com>
2023-04-03 21:39:21 +0200

    Updated gitignore and removed compile.sh

--- src/compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -O3 -Wall -shared -std=c++20 -fPIC -I/usr/include/python3.10 -I/home/simone/.local/lib/python3.10/site-packages/pybind11/include ./binding.cc -o VanDerPol.cpython-310-x86_64-linux-gnu.so
