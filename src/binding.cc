6f29c76dd1fdfcc8875b1d15f8fd6f22f3f9be76
sbaldu <simone.balducci00@gmail.com>
2023-04-04 08:25:09 +0200

    Added solver class for Runge-Kutta method

--- src/binding.cc
+++ src/binding.cc
@@ -6,8 +6,15 @@
 
 #include "eqDiffSolver.h"
 
-solution vdp(double x0, double y0, double mu, float dt, double max_t) {
-  eulerSolver solver([mu](double x) { return -mu*(1 - x*x); }, [](double x) { return x; });
+solution vdp_euler(double x0, double y0, double mu, float dt, double max_t) {
+  eulerSolver solver([mu](double x) { return -mu * (1 - x * x); }, [](double x) { return x; });
+  solver.solve(x0, y0, dt, max_t);
+
+  return solver.sol();
+}
+
+solution vdp_rk2(double x0, double y0, double mu, float dt, double max_t) {
+  rk2Solver solver([mu](double x) { return -mu * (1 - x * x); }, [](double x) { return x; });
   solver.solve(x0, y0, dt, max_t);
 
   return solver.sol();
@@ -17,12 +24,13 @@ PYBIND11_MODULE(VanDerPol, m) {
   m.doc() = "Numerical solver of the VanDerPol equation";
 
   pybind11::class_<solution>(m, "solution")
-	.def(pybind11::init<>())
-	.def(pybind11::init<int>())
-	.def(pybind11::init<int, int>())
-	.def("setCapacity", &solution::setCapacity)
-	.def("getVariable_i", &solution::getVariable_i)
-	.def("getTime", &solution::getTime);
+      .def(pybind11::init<>())
+      .def(pybind11::init<int>())
+      .def(pybind11::init<int, int>())
+      .def("setCapacity", &solution::setCapacity)
+      .def("getVariable_i", &solution::getVariable_i)
+      .def("getTime", &solution::getTime);
 
-  m.def("vdp", &vdp);
+  m.def("vdp_euler", &vdp_euler);
+  m.def("vdp_rk2", &vdp_rk2);
 }
--- src/eqDiffSolver.h
+++ src/eqDiffSolver.h
@@ -16,38 +16,34 @@ struct solution {
   // Constructors
   solution() = default;
   solution(int n_) : n{n_} {}
-  solution(int n_, int capacity_) : n{n_}, Capacity{capacity_} {
-	data.resize(n * Capacity);
-  }
+  solution(int n_, int capacity_) : n{n_}, Capacity{capacity_} { data.resize(n * Capacity); }
 
-  void setCapacity(int capacity) { 
-	data.resize(n * capacity); 
-	Capacity = capacity;
+  void setCapacity(int capacity) {
+    data.resize(n * capacity);
+    Capacity = capacity;
   }
 
   template <typename T1, typename... Tn>
   void add(int i, T1 arg1, Tn... args) {
-	data[i] = arg1;
-	add(i + Capacity, args...);
+    data[i] = arg1;
+    add(i + Capacity, args...);
   }
 
   void add(int i) {}
 
-  std::vector<double>::iterator it_to_variable(int i) {
-	return data.begin() + Capacity * i;
-  }
+  std::vector<double>::iterator it_to_variable(int i) { return data.begin() + Capacity * i; }
 
   std::vector<double> getVariable_i(int i) {
-	std::vector<double> slice;
-	std::copy(it_to_variable(i), it_to_variable(i) + Capacity, std::back_inserter(slice));
-	
-	return slice;
+    std::vector<double> slice;
+    std::copy(it_to_variable(i), it_to_variable(i) + Capacity, std::back_inserter(slice));
+
+    return slice;
   }
   std::vector<double> getTime() {
-	std::vector<double> slice;
-	std::copy(it_to_variable(n - 1), it_to_variable(n - 1) + Capacity, std::back_inserter(slice));
-	
-	return slice;
+    std::vector<double> slice;
+    std::copy(it_to_variable(n - 1), it_to_variable(n - 1) + Capacity, std::back_inserter(slice));
+
+    return slice;
   }
 };
 
@@ -71,17 +67,57 @@ public:
     double steps{max_t / dt};
     double z;
 
-	m_sol.n = 3;
+    m_sol.n = 3;
     m_sol.setCapacity(steps + 1);
     m_sol.add(0, x0, y0, 0.);
 
     for (int i{1}; i <= steps; ++i) {
       z = x + y * dt;
-      y = -(m_ax(x) * y + m_bx(x))* dt + y;
+      y = -(m_ax(x) * y + m_bx(x)) * dt + y;
       x = z;
-      t += dt;
 
-	  m_sol.add(i, x, y, t);
+      t += dt;
+      m_sol.add(i, x, y, t);
+    }
+  }
+};
+
+class rk2Solver {
+private:
+  coeff_t m_ax;
+  coeff_t m_bx;
+  solution m_sol;
+
+public:
+  rk2Solver() = default;
+  rk2Solver(coeff_t ax, coeff_t bx) : m_ax{std::move(ax)}, m_bx{std::move(bx)} {}
+
+  solution sol() const { return m_sol; }
+
+  void solve(double x0, double y0, double dt, double max_t) {
+    double x{x0};
+    double y{y0};
+    double t{};
+    double steps{max_t / dt};
+    // Coefficients for the Runge-Kutta method
+    double k1, k2, l1, l2;
+    double z;
+
+    m_sol.n = 3;
+    m_sol.setCapacity(steps + 1);
+    m_sol.add(0, x0, y0, 0.);
+
+    for (int i{}; i <= steps; ++i) {
+      k1 = dt * y;
+      l1 = -dt * (m_ax(x) * y + m_bx(x));
+      k2 = dt * (y + l1);
+      z = x + (k1 + k2) / 2;
+      l2 = -dt * (m_ax(x + k1) * (y + l1 * dt / 2) + m_bx(x + k1));
+      y = y + (l1 + l2) / 2;
+      x = z;
+
+      t += dt;
+      m_sol.add(i, x, y, t);
     }
   }
 };
